24268b1ff2a2f08a736fa4c74a60963a093cf647
migu3ln <kalexmiguelalba@gmail.com>
2016-10-11 18:45:57 -0500

    adadd
    listo coasas

--- chat.js
+++ chat.js
@@ -18,9 +18,9 @@ var port_mysql = 3306;
 var puerto_io = 3000;
 //---BDD SERVER---
 //LOCALMENTE
-//var params_bdd = {user: "root", password: "", host: "localhost", port: port_mysql, database: "xywer"};
+var params_bdd = {user: "root", password: "", host: "localhost", port: port_mysql, database: "xywer"};
 //LOCALMENTE-PUBLICA
-var params_bdd = {user: "root", password: "", host: "186.101.18.195", port: port_mysql, database: "xywer"};
+//var params_bdd = {user: "root", password: "", host: "186.101.18.195", port: port_mysql, database: "xywer"};
 //BDD CREATIVEWEB
 //var params_bdd = {user: "pekesc5_meetclic", password: "meetclic@", host: "creativeweb.com.ec", port: port_mysql, database: "pekesc5_xywer"};
 //*********************MYSQL*****************
@@ -32,6 +32,7 @@ var PORT = process.env.PORT || port_listen;
 var mysql = require('mysql');//para la comunicacion con la bdd 
 var cors = require('cors');//EL NOS FACILITA LA COMUNICACION A ESAS URLS  ACCESO A ESA URL
 app.use(cors());
+
 app.get('/', function (req, res) {
     //request : son cabeceras y datos que nos envia el navegador.
     //response : son todo lo que enviamos desde el servidor.
@@ -62,6 +63,7 @@ app.get('/user/:id', function (req, res) {
 });
 //---GESTION D INFORMACION--
 app.get('/registrePersonaInformacion', function (req, res, next) {
+    var post = req.query;
     connection = mysql.createConnection(params_bdd);
     connection.connect(function (err) {
         if (err) {
@@ -75,47 +77,33 @@ app.get('/registrePersonaInformacion', function (req, res, next) {
 
     });
     var result = [];
-    var post = req.query;
+    console.log(post);
 
     if (!post.id) {//crear nuevo
-        var phone_number = post.phone_number;
-        var query_string = "SELECT * FROM  " + cuenta_persona + " t  where  t.phone_number='" + phone_number + "'";
-        var objec_conection_bdd = connection;
-        var params_data = {query_string: query_string, objec_conection_bdd: objec_conection_bdd};
-        getDataModel(params_data, function (data) {
-            if (data.length == 0) {//validacion d informacion si no existe datos se puede registrar al sistema de qullqi cash
-                var data_save = {nombres: post.nombres, apellidos: post.apellidos, persona_genero_id: post.persona_genero_id};
-                var query = connection.query('INSERT INTO ' + persona + ' SET ?', data_save, function (err, result) {
-                    var persona_id = result.insertId;
-                    var data_save = {phone_number: post.phone_number, documento: post.documento, entidad_data_id: entidad_data_id, persona_id: persona_id, pass_user: post.pass_user};
-                    var query2 = connection.query('INSERT INTO ' + cuenta_persona + ' SET ?', data_save, function (err, result2) {
-                        var children_id = result2.insertId;
-                        var data_result = {id: children_id};
-                        res.json({success: true, data: data_result, msj: 'Se registro correctamente!!'});
-                    });
-                });
-
-            } else {
-                result = {
-                    success: false,
-                    msj: "El # Tlfn ya fue tomado" + phone_number
-                };
-                res.json(result);
-
-            }
-
+        var table_save = "persona";
+        var data_save = {nombres: post.nombres, apellidos: post.apellidos, persona_genero_id: post.genero_id};
+        var query_save_persona = connection.query('INSERT INTO ' + table_save + ' SET ?', data_save, function (err, result) {
+            var table_save = "persona_informacion";
+            var persona_id = result.insertId;
+            console.log(result, err);
+            var data_save = {compania_taxis: post.compania_taxis, ciudad: post.ciudad, persona_id: persona_id};
+            var query_save_persona_informacion = connection.query('INSERT INTO ' + table_save + ' SET ?', data_save, function (err, result2) {
+                var children_id = result2.insertId;
+                var data_result = {id: children_id, nombres: post.nombres, apellidos: post.apellidos, persona_genero_id: post.persona_genero_id, compania_taxis: post.compania_taxis, ciudad: post.ciudad, persona_id: persona_id};
+                res.json({success: true, data: data_result, msj: 'Se registro correctamente!!'});
+            });
         });
 
     } else {
-        var queryString = 'UPDATE  persona_informacion SET nombres="' + post.nombres + '",' + 'apellidos="' + post.apellidos + '",' + 'documento="' + post.documento + '" WHERE persona_informacion.id=' + post.id;
-
+        var queryString = 'UPDATE  persona SET nombres="' + post.nombres + '",' + 'apellidos="' + post.apellidos + '",' + 'persona_genero_id="' + post.persona_genero_id + '" WHERE persona.id=' + post.id;
         connection.query(queryString, function (err, result) {
 
-            var data = {id: post.id, nombres: post.nombres, apellidos: post.apellidos, documento: post.documento}
-            res.json({success: true, data: data, update: true});
+        });
+        var queryString = 'UPDATE  persona_informacion SET compania_taxis="' + post.compania_taxis + '",' + 'ciudad="' + post.ciudad + '" WHERE id.id=' + post.persona_id;
+        connection.query(queryString, function (err, result) {
+
         });
     }
-
 
 });
 
@@ -131,20 +119,18 @@ app.get('/getPersonaInformacion', function (req, res, next) {
             errors = err;
             init_bdd = false;
             console.log("--------------------erro---------------------");
-
             res.json({success: false, data: post, init_bdd: init_bdd, "msj-error-bdd": errors});
 
         } else {
-//            var query_string = "SELECT t.id key_id,t.persona_id, pg.valor genero,p.nombres,p.apellidos ,t.compania_taxis,t.ciudad FROM persona_informacion  t ";
-//            query_string += "inner join persona p on t.persona_id=p.id ";
-//            query_string += "inner join persona_genero pg on p.persona_genero_id=pg.id  ";
-//            var objec_conection_bdd = connection;
-//            var params_data = {query_string: query_string, objec_conection_bdd: objec_conection_bdd};
-//            getDataModel(params_data, function (data) {
-//                res.json(data);
-//
-//            });
-            res.json({success: false,"listo":"ñladekmadmadm"});
+            var query_string = "SELECT t.id id,t.persona_id, pg.valor genero,p.nombres,p.apellidos ,t.compania_taxis,t.ciudad FROM persona_informacion  t ";
+            query_string += "inner join persona p on t.persona_id=p.id ";
+            query_string += "inner join persona_genero pg on p.persona_genero_id=pg.id  ";
+            var objec_conection_bdd = connection;
+            var params_data = {query_string: query_string, objec_conection_bdd: objec_conection_bdd};
+            getDataModel(params_data, function (data) {
+                res.json(data);
+
+            });
 
         }
 
@@ -210,7 +196,7 @@ function initBdd() {
         connection = mysql.createConnection(params_bdd);
         connection.connect(function (err) {
             if (err) {
-                console.log('Error connecting to Db:',err);
+                console.log('Error connecting to Db:', err);
                 result.push({"success": false, errors: err});
             } else {
                 init_bdd = true;
